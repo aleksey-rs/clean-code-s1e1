86e88ef6a129721ed694fa696259c002374cbc6f
aleksey-rs <alekseyrs65@gmail.com>
2021-04-01 05:28:36 +0300

    fix: replace tag selectors with class selectors as per the fix according to rule 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -33,18 +33,21 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task';
+    label.className='task item-label';
+    listItem.className='item';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.className="item-checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="task item-text";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="btn edit";
 
-    deleteButton.className="delete";
+    deleteButton.className="btn delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className="delete-img";
     deleteButton.appendChild(deleteButtonImg);
 
 
